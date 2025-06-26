5af18dc6599a6f9d2be9589903153dc35c8380a1
Chauhan Dhruvil <chauhandhruvil12@gmail.com>
2025-06-26 23:17:32 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,6 +2,8 @@
 import { API_BASE_URL } from './apiConfig';
 import { io } from 'socket.io-client';
 
-const socket = io(API_BASE_URL.replace('/api', ''));
-
+const socket = io(API_BASE_URL.replace('/api', ''), {
+  transports: ['websocket'],
+  withCredentials: true
+});
 export default socket;
